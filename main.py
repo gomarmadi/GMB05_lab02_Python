2ec4c9c14dd1b0bc90e3d35c8b71854132eee3ef
Madi <71094011+gomarmadi@users.noreply.github.com>
2022-09-19 16:38:13 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import math
+import sys
+
+from PyQt5 import QtGui
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+
+
+class Main(QWidget):
+    def __init__(self):
+        super(Main, self).__init__()
+        loadUi('main.ui', self)
+
+        self.setWindowTitle('GMB05_lab02')
+        self.setWindowIcon(QtGui.QIcon('logo.png'))
+
+        self.label_img.setPixmap(QPixmap('Main.png'))
+        self.label_img.setScaledContents(True)
+
+        self.btn_solve.clicked.connect(self.solve)
+        self.btn_clear.clicked.connect(self.clear)
+        self.btn_exit.clicked.connect(self.close)
+
+    def solve(self):
+        try:
+            a = float(self.lineEdit_a.text())
+            b = float(self.lineEdit_b.text())
+            x = float(self.lineEdit_x.text())
+            if x >= 5:
+                answer = x * (math.pow(a,2) + math.pow(b,2)) / 6 * a
+            else:
+                answer = 6 * a * b - 5 * x
+            self.label_answer.setText('Ответ: ' + str(format(answer, '.2f')))
+        except:
+            self.label_answer.setText(
+                'Ошибка!')
+
+    def clear(self):
+        self.lineEdit_a.setText('')
+        self.lineEdit_b.setText('')
+        self.lineEdit_x.setText('')
+        self.label_answer.setText('Ответ: ')
+
+
+app = QApplication(sys.argv)
+window = Main()
+window.show()
+sys.exit(app.exec_())
